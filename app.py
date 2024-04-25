3b9e17d63a2a75a098593995d9e344fe3db8b003
kennethctk <167070561+kennethctk@users.noreply.github.com>
2024-04-25 12:00:48 +0000

    sync 5 (missing [form])

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
+
 @app.route("/", methods=["GET", "POST"])
 def home():
-    user = request.args.get('user')
-    un = request.args.get("un")
-    pw = request.args.get("pw")
-    if user == None:
+    if request.method == "GET":
         return render_template("login page 1.html")
-    elif un == "bob" and pw == "123":
-        return "Hello " + un
     else:
-        return "User not recognised!"
+        un = request.args.get("un")
+        pw = request.args.get("pw")
+        if un == None:
+            return render_template("login page 1.html")
+        elif un == "bob" and pw == "123":
+            return "Hello " + un
+        else:
+            return "User not recognised!"
