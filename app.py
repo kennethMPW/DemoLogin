2faa6950fedb0ee4c62f5e3b354abcaaded88731
kennethctk <167070561+kennethctk@users.noreply.github.com>
2024-04-25 11:55:42 +0000

    sync 4 (doesn't return hello page)

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
     user = request.args.get('user')
+    un = request.args.get("un")
+    pw = request.args.get("pw")
     if user == None:
         return render_template("login page 1.html")
     elif un == "bob" and pw == "123":
