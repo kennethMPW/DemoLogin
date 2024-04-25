37d5b8dc3b24e64ac718715be17366799e092bff
kennethctk <167070561+kennethctk@users.noreply.github.com>
2024-04-25 11:51:27 +0000

    sync 2

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 from flask import Flask, render_template, request
-
 app = Flask(__name__)
 @app.route("/")
 def home():
     user = request.args.get('user')
     if user == None:
         return render_template("login page 1.html")
-    elif user=="bob":
-        return "Hello " + user
+    elif un == "bob" and pw == "123":
+        return "Hello " + un
     else:
         return "User not recognised!"
